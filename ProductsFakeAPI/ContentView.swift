f1930c5b3a450b0266b002c69d842fadd55359fe
qlap04 <uchihaquoc04@gmail.com>
2024-07-07 16:42:56 +0700

    Initial Commit

---
+++ ProductsFakeAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProductsFakeAPI
+//
+//  Created by Phan Quốc Lập on 07/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProductsFakeAPI/ProductsFakeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsFakeAPIApp.swift
+//  ProductsFakeAPI
+//
+//  Created by Phan Quốc Lập on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductsFakeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
